15ed560c38663420d7af3e29c4ffecbf0c9e8e8a
Donut <>
2021-09-22 10:40:12 +0700

    add menu view model

--- DemoNut/Scene/Menu/MenuViewModel.swift
+++ DemoNut/Scene/Menu/MenuViewModel.swift
@@ -17,16 +17,47 @@ class MenuViewModel {
     
     /// Variable Sender
     var sender: Sender?
+    var menuList = [MenuInfo]()
     
     // Code here to create your view model
 }
 
 extension MenuViewModel {
 
-    // Code here to call data
-    func requestData(_ completion: (() -> ())? = nil) {
+    func fetchMenu(_ completion: (() -> ())? = nil) {
         
-        // callback
+        menuList.append(MenuInfo(title: "หน้าแรก",
+                                 icon: UIImage.Theme.ic_tabbar_home,
+                                 menu: .Home))
+        
+        menuList.append(MenuInfo(title: "ข้อมูลส่วนตัว",
+                                 icon: UIImage.Theme.ic_tabbar_home,
+                                 menu: .Profilem))
+        
+        menuList.append(MenuInfo(title: "ประวัติการลงเวลาปฏิบัติงาน",
+                                 icon: UIImage.Theme.ic_tabbar_home,
+                                 menu: .Stamp))
+        
+        menuList.append(MenuInfo(title: "การทำงานล่วงเวลา (OT)",
+                                 icon: UIImage.Theme.ic_tabbar_home,
+                                 menu: .OT))
+        
+        menuList.append(MenuInfo(title: "การลา",
+                                 icon: UIImage.Theme.ic_tabbar_home,
+                                 menu: .Leave))
+        
+        menuList.append(MenuInfo(title: "ปฏิทิน",
+                                 icon: UIImage.Theme.ic_tabbar_home,
+                                 menu: .Calendar))
+        
+        menuList.append(MenuInfo(title: "ข่าวสารองค์กร",
+                                 icon: UIImage.Theme.ic_tabbar_home,
+                                 menu: .News))
+        
+        menuList.append(MenuInfo(title: "ออกจากระบบ",
+                                 icon: UIImage.Theme.ic_tabbar_home,
+                                 menu: .Logout))
+        
         completion?()
     }
 }
